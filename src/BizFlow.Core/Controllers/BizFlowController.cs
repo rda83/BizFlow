10db683d5dd1b393c51c2c540559d9218b7259f9
denis <rda198324@google.com>
2025-03-26 16:32:22 +0700

    Add project files.

---
+++ src/BizFlow.Core/Controllers/BizFlowController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BizFlow.Core.Controllers
+{
+    [ApiController]
+    [Route("bizFlow")]
+    public class BizFlowController : ControllerBase
+    {
+        public BizFlowController() { }
+
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public IActionResult GetOperations()
+        {
+            return Ok("Biz flow begin ...");
+        }
+    }
+}
---
+++ src/BizFlow.Core/Services/DI/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using BizFlow.Core.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BizFlow.Core.Services.DI
+{
+    public static class ServiceRegistration
+    {
+        public static void AddBizFlow(this IServiceCollection services)
+        {
+            var assembly = typeof(BizFlowController).GetTypeInfo().Assembly;
+
+            services.AddMvcCore()
+                .AddApplicationPart(assembly);
+        }
+    }
+}
