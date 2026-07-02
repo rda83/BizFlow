fd4c1a62a7acd92d5f9ef6759eab1436bc1a72a2
Denis Rostovtsev <rda83@mail.ru>
2026-07-02 09:02:12 +0700

    +CronSchedule

---
+++ src/BizFlow.Schedules.Cron/CronSchedule.cs
@@ -0,0 +1,26 @@
+﻿using BizFlow.Abstractions;
+using Cronos;
+
+namespace BizFlow.Schedules.Cron
+{
+    public class CronSchedule : ISchedule
+    {
+        private readonly CronExpression _expression;
+        private readonly TimeZoneInfo _timeZone;
+
+        public CronSchedule(string cronExpression, TimeZoneInfo? timeZone = null)
+        {
+            _expression = CronExpression.Parse(cronExpression);
+            _timeZone = timeZone ?? TimeZoneInfo.Utc;
+        }
+
+        public DateTimeOffset? GetNextRun(DateTimeOffset? lastRun, DateTimeOffset now)
+        {
+            if (lastRun == null)
+            {
+                return _expression.GetNextOccurrence(now, _timeZone);
+            }
+            return _expression.GetNextOccurrence(lastRun.Value, _timeZone);
+        }
+    }
+}
